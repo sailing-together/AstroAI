adee041cad9cd4997ca1fc6ad183f4ee0180e121
Yvonne Yao <yvonne.yao278@gmail.com>
2025-06-29 20:25:19 +1000

    delete old code

--- chatgpt-examples/backend/compatibility_game/UserInput.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel, Field  
-
-class UserInput(BaseModel):
-    sign_1: str = Field(..., description="The input sign used for compability.")
-    sign_2: str = Field(..., description="The input sign used for compability.")
